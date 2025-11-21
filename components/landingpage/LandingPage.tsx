eb9b1a878e4e312505695d6896534f9144c4b59f
sanketparajuli <parajulisanket7@gmail.com>
2025-11-21 13:22:31 +0545

    chnages

--- components/landingpage/LandingPage.tsx
+++ components/landingpage/LandingPage.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
-const images = ["hero.png", "hero.png", "hero.png"];
+const images = ["/hero.png", "/hero.png", "/hero.png"];
 
 export default function LandingPage() {
   const [current, setCurrent] = useState(0);
